be2ae44d906a64064c220de3c04b74d84527d620
Dhaval Shah <dhavalmshah@gmail.com>
2023-09-21 21:40:34 +0200

    added sudo as required

--- tamer.sh
+++ tamer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update package lists
-apt-get update
+sudo apt-get update
 
 # Install git
-apt-get install git -y
+sudo apt-get install git -y
 
 # Install docker using convenience script
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -20,7 +20,7 @@ source ~/.nvm/nvm.sh
 nvm install node 18
 
 # Install nginx
-apt-get install nginx -y
+sudo apt-get install nginx -y
 
 # Install certbot
 sudo apt-get install certbot python3-certbot-nginx
